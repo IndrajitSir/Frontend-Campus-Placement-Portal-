6260af6222ba27a9e39fc16d3f0e66893e7d5608
IndrajitSir <indrajitmandal779@gmail.com>
2025-04-21 22:03:53 +0530

    Declared routes in main file and created a visually apealling landing page

---
+++ src/App.jsx
@@ -0,0 +1,127 @@
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+// Components
+import FloatingBlob from "./AnimatedComponents/FloatingBlob.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import AboutSection from "./components/AboutSection/AboutSection.jsx";
+import PlacementStats from "./components/PlacementStats/PlacementStats.jsx";
+import Testimonials from "./components/Testimonials/Testimonials.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import CompaniesSection from "./components/Companies/CompaniesSection.jsx";
+// Define animation
+import { animation } from "./constants/constants.js";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <FloatingBlob moveAnimation={animation} />
+        <Navbar />
+        <Main>
+          <Moto>
+            <motion.h1
+              initial={{ opacity: 0, y: -20 }}
+              whileInView={{ color: "black", opacity: 1 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              Connect . Apply
+            </motion.h1>
+          </Moto>
+          <GradientText>
+            <motion.h1
+              initial={{ opacity: 0, y: -20 }}
+              whileInView={{ opacity: 1 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.2 }}
+            >
+              Get HIRED
+            </motion.h1>
+          </GradientText>
+          <motion.p
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ color: "black", opacity: 1 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.4 }}
+          >
+            A one-stop platform for students and companies to connect.
+          </motion.p>
+          <ExploreButton to="/home/dashboard">Explore</ExploreButton>
+        </Main>
+        <OtherSections>
+          <div className="bg-white text-black p-6 font-sans">
+            {/* Placement Statistics Section */}
+            <PlacementStats />
+            {/* Companies Section */}
+            <CompaniesSection />
+            {/* About Placement Portal Section */}
+            <AboutSection />
+            {/* Success Stories Section */}
+            <Testimonials />
+            {/* Footer Section */}
+            <Footer />
+          </div>
+        </OtherSections>
+      </Container>
+    </>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+  min-height: 100vh;
+  background: white;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Main = styled.div`
+  min-height: 80vh;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const OtherSections = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const ExploreButton = styled(Link)`
+  padding: 12px 24px;
+  background: linear-gradient(45deg, #ff79c6, #bd93f9);
+  border-radius: 10px;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+  margin-top: 20px;
+`;
+const Moto = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h1 {
+  font-Size: 5rem;
+  font-style: bold;
+  position: relative;
+  bottom: -20px;
+  }
+`;
+const GradientText = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h1 {
+  font-size: 4rem;
+  font-weight: bold;
+  background: linear-gradient(45deg, #ff79c6, #bd93f9);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  }
+`;
---
+++ src/main.jsx
@@ -0,0 +1,63 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+// -- PAGES imported
+import App from './App.jsx'
+import Auth from './pages/Auth/Auth.jsx'
+import Placements from './pages/Placements/Placements.jsx'
+import ProtectedRoute from './functionality/ProtectedRoutes.jsx'
+import Dashboard from './pages/Dashboard/Dashboard.jsx'
+import DashBoardContent from './pages/Dashboard/DashBoardContent'
+import ProfilePage from './pages/Profile/Profile.jsx'
+import Students from './pages/Users/Students/Students'
+import ShortlistedApplications from './pages/Applications/ShortlistedApplications/ShortlistedApplications'
+import AppliedApplications from './pages/Applications/AppliedApplications/AppliedApplications';
+import RejectedApplications from './pages/Applications/RejectedApplications/RejectedApplications'
+import SelectedApplications from './pages/Applications/SelectedApplications/SelectedApplications'
+import ManageApplications from './pages/Manage_Applications/ManageApplications'
+import ManageUsers from './pages/ManageUsers/ManageUsers'
+import AppliedForJobs from './components/AppliedForJobs/AppliedForJobs'
+import MonitorSystem from './components/Dashboards/Admin_dashboard/Monitor_System/MonitorSystem'
+// CONTEXT PROVIDER imported
+import { AuthCrediantialsProvider } from './context/AuthContext/AuthProvider.jsx'
+import { PlacementDataProvider } from './context/PlacementContext/PlacementProvider.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<App />} />
+      <Route>
+        <Route path="/login" element={<Auth />} />
+        <Route path="/register" element={<Auth />} />
+        <Route path="/home" element={<ProtectedRoute />} >
+          <Route path="" element={<PlacementDataProvider><Placements /></PlacementDataProvider>} />
+          <Route path="placements" element={<PlacementDataProvider><Placements /></PlacementDataProvider>} />
+          <Route path="dashboard" element={<Dashboard />} >
+            <Route path="" element={<DashBoardContent />} />
+            <Route path="profile" element={<ProfilePage />} />
+            <Route path="students" element={<Students />} />
+            <Route path="applied-jobs" element={<AppliedForJobs />} />
+            <Route path="manage-users" element={<ManageUsers />} />
+            <Route path="monitor-system" element={<MonitorSystem />} />
+            <Route path="manage-applications">
+              <Route path="" element={<ManageApplications />} />
+              <Route path="selected-candidates" element={<SelectedApplications />} />
+              <Route path="applied-candidates" element={<AppliedApplications />} />
+              <Route path="shortlisted-candidates" element={<ShortlistedApplications />} />
+              <Route path="rejected-candidates" element={<RejectedApplications />} />
+            </Route>
+          </Route>
+        </Route>
+      </Route >
+    </Route >
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthCrediantialsProvider>
+      <RouterProvider router={router} />
+    </AuthCrediantialsProvider>
+  </StrictMode>,
+)
