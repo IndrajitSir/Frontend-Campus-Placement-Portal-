1e3b13b95286dd0b929c14e3ce9793d6d9afa388
IndrajitSir <indrajitmandal779@gmail.com>
2025-04-21 23:10:05 +0530

    added dashboard for admins and super admin

---
+++ src/Components/Dashboards/Admin_dashboard/Admin_Dashboard.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+// Shadcn Components
+import { Card, CardHeader, CardContent, CardTitle } from '../../ui/card'
+import { Button } from '../../ui/button';
+// Components
+import SystemStatus from '../../../components/System_Status/SystemStatus.jsx';
+
+function Admin_Dashboard() {
+    const navigate = useNavigate();
+    return (
+        <>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 w-full">
+                <Card className="w-90">
+                    <CardHeader>
+                        <CardTitle>User Management</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <Button className="mt-2 cursor-pointer" onClick={() => { navigate("/home/dashboard/manage-users") }}>Manage Users</Button>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>System Monitoring</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <Button className="mt-2 cursor-pointer" onClick={() => { navigate("/home/dashboard/monitor-system") }}>View Logs</Button>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Placement Records</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <Button className="mt-2 cursor-pointer" onClick={() => { navigate("/home/placements") }}>Check Records</Button>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Applications Management</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <Button className="mt-2 cursor-pointer" onClick={() => { navigate("/home/dashboard/manage-applications") }}>Manage Applications</Button>
+                    </CardContent>
+                </Card>
+            </div>
+            <div>
+                <SystemStatus />
+            </div>
+        </>
+    )
+}
+
+export default Admin_Dashboard
