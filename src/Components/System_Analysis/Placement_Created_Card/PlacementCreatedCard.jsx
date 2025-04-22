9936e787d1c847f282e0d7754e58536ebe9fed7c
IndrajitSir <indrajitmandal779@gmail.com>
2025-04-22 17:15:32 +0530

    added Placement Created Card

---
+++ src/Components/System_Analysis/Placement_Created_Card/PlacementCreatedCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+// Chart
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+// Shadcn Components
+import { Card, CardContent } from "../../../components/ui/card";
+// Hooks
+import { usePlacementsCreatedPerMonth } from '../../../hooks/Analytics/useAnalytics.js';
+// Components
+import CircleLoader from '../../../components/Loader/CircleLoader.jsx';
+function PlacementCreatedCard() {
+    const { data, loading, error } = usePlacementsCreatedPerMonth();
+    if (loading) return <CircleLoader />;
+    if (error) return <p>Error fetching Placements Created Per Month analysis data!</p>;
+    if (!Array.isArray(data)) return <p>No data available.</p>;
+    return (
+        <>
+            {/* 4. Placements Created per Month */}
+            <Card className="w-full max-w-xl mx-auto shadow-md">
+                <CardContent>
+                    <h2 className="text-xl font-semibold mb-2">Placements per Month</h2>
+                    <BarChart width={450} height={250} data={data}>
+                        <XAxis dataKey="_id" />
+                        <YAxis />
+                        <Tooltip />
+                        <Bar dataKey="totalPlacements" fill="#ffc658" radius={[4, 4, 0, 0]} />
+                    </BarChart>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
+
+export default PlacementCreatedCard
