97930de29f6eb1bb736d56464fb14e73bae9d8db
IndrajitSir <indrajitmandal779@gmail.com>
2025-04-22 17:17:15 +0530

    added Selected Students Card

---
+++ src/Components/System_Analysis/Selected_Students_Card/SelectedStudentsCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+// Chart
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+// Shadcn Components
+import { Card, CardContent } from "../../../components/ui/card";
+// Hooks
+import { useSelectedStudentsPerDepartment } from '../../../hooks/Analytics/useAnalytics.js';
+// Components
+import CircleLoader from '../../../components/Loader/CircleLoader.jsx';
+function SelectedStudentsCard() {
+    const { data, loading, error } = useSelectedStudentsPerDepartment();
+
+    if (loading) return <CircleLoader />;
+    if (error) return <p>Error fetching Selected Students Per Department analysis data!</p>;
+    if (!Array.isArray(data)) return <p>No data available.</p>;
+    return (
+        <>
+            {/* 3. Selected Students by Department */}
+            <Card className="w-full shadow-md">
+                <CardContent>
+                    <h2 className="text-xl font-semibold mb-2">Selected Students per Department</h2>
+                    <BarChart width={1300} height={250} data={data}>
+                        <XAxis dataKey="_id" />
+                        <YAxis allowDecimals={false} />
+                        <Tooltip />
+                        <Bar dataKey="selected" fill="#ff7300" radius={[4, 4, 0, 0]}/>
+                        <Bar dataKey="applied" fill="#ff7300" radius={[4, 4, 0, 0]}/>
+                        <Bar dataKey="shortlisted" fill="#ff7300" radius={[4, 4, 0, 0]}/>
+                        <Bar dataKey="rejected" fill="#ff7300" radius={[4, 4, 0, 0]}/>
+                    </BarChart>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
+
+export default SelectedStudentsCard
