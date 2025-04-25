a4b7d984972cb741332eb5bb14ac7e066f887258
IndrajitSir <indrajitmandal779@gmail.com>
2025-04-25 19:55:01 +0530

    added a dialog for delete placement post

---
+++ src/Dialog/DeletePlacement_dialog/DeletePlacementDialog.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { toast } from 'react-toastify';
+// Shadcn Components
+import { Dialog, DialogContent, DialogHeader, DialogFooter, DialogTitle } from "../../components/ui/dialog";
+import { Button } from '../../components/ui/button';
+// CONTEXT api
+import { useUserData } from '../../context/AuthContext/AuthContext.jsx';
+// Circle Loader
+import CircleLoader from '../../components/Loader/CircleLoader';
+// Environment variable
+const API_URL = import.meta.env.VITE_API_URL;
+
+export default function DeletePlacementPostDialog({
+    deletePlacementPostDialog, setDeletePlacementPostDialog,
+    placementPostID, onPostDelete, loading, setLoading
+}) {
+    const { accessToken } = useUserData();
+    const handlePlacementPostDelete = async () => {
+        setLoading(true);
+        const res = await fetch(`${API_URL}/api/v1/placements/${placementPostID}`, {
+            method: "DELETE",
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+        });
+        const response = await res.json();
+        console.log("response delete post: ", response);
+        if (!response.success) {
+            toast.warning(response.message)
+        }
+        toast.success(response.message);
+        onPostDelete(response.data._id);
+    }
+    return (
+        <>
+            <Dialog open={deletePlacementPostDialog} onOpenChange={setDeletePlacementPostDialog}>
+                <DialogContent>
+                    <DialogTitle>Delete Placement</DialogTitle>
+                    <DialogHeader>Are you sure you want to Delete?</DialogHeader>
+                    <DialogFooter>
+                        <Button className="cursor-pointer" variant="secondary" onClick={() => { setLoading(false); setDeletePlacementPostDialog(false) }}>Cancel</Button>
+                        <Button className="cursor-pointer" variant="destructive" onClick={handlePlacementPostDelete}>{loading ? <CircleLoader /> : "Delete"}</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
+    )
+}
